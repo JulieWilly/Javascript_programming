2410599e6b2ef4b961253b9afb9fafdb620e66a0
JulieWilly <logincoin40@gmail.com>
2023-01-14 22:03:51 +0300

    How t ouse arrow functions in ECMACSScript 6

---
+++ es6-es2015/es6-arrow_functions.js
@@ -0,0 +1,27 @@
+/////////////////////////////////////////////
+// ARROW FUNCTIONS
+
+const years = [1990, 1999, 1960, 1970, 2001, 1990];
+
+// ES5
+var ages5 = years.map(function(el) {
+    return 2016 - el;
+});
+
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(el => 2016 - el);
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}.`
+});
+
+console.log(ages6);
+
